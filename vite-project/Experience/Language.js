44c86a5f33216f5d3c8b687e184f7d8cc2ed7ed6
YORIKO <anandaindratan85@gmail.com>
2022-07-22 13:10:39 +0700

    language button

---
+++ vite-project/Experience/Language.js
@@ -0,0 +1,22 @@
+import { EventEmitter } from "events";
+
+export default class Language extends EventEmitter {
+    constructor() {
+        super();
+
+        this.toggleButtonLang= document.querySelector(".toggle-button-language");
+        this.toggleCircleLang = document.querySelector(".toggle-circle-language");
+
+
+        this.setEventListeners();
+    }
+
+    setEventListeners() {
+        this.toggleButtonLang.addEventListener("click", () => {
+            this.toggleCircleLang.classList.toggle("slide");
+            // console.log(this.theme);
+
+            // this.emit("switch", this.theme);
+        });
+    }
+}
